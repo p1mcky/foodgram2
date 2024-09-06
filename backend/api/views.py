6ab3644d1abe78c330a1bfa1b995af3a419b4df7
Suponin Kirill <kirillsuponinqwerty@gmail.com>
2024-09-06 16:07:49 +0300

    sec pu

--- backend/api/views.py
+++ backend/api/views.py
@@ -109,7 +109,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     @action(detail=False,
             methods=['get'],
-            permission_classes=[IsAuthenticated])
+            permission_classes=[IsAuthenticated]
+        )
     def download_shopping_cart(self, request, **kwargs):
         file_name = 'shopping_cart.txt'
 
